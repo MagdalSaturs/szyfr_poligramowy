c4f3cc381002fd4c5ead8af3ece13dbbe82b94a0
magdalena <u31_magsat_waw@technischools.com>
2022-12-31 10:52:33 +0100

    Kod

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './App.scss';
 
 function App() {
+  const [tekst, setTekts] = React.useState([]);
+  const text = React.createRef();
+
+  const Code = (e) => {
+    const textValue = text.current.value;
+    const alphabet = 'abcdefghijklmnopqrstuvwxyz';
+
+    const code = (textValue) => {
+      let result = '';
+      for (let i = 0; i < textValue.length; i++) {
+        if (textValue[i] === ' ') {
+          result += ' ';
+        } else {
+          const index = alphabet.indexOf(textValue[i]);
+          result += alphabet[index + 1];
+        }
+      }
+      return result;
+    }
+    setTekts(code(textValue));
+    console.log(code(textValue));
+  };
+
+  const Decode = (e) => {
+    const textValue = text.current.value;
+    const alphabet = 'abcdefghijklmnopqrstuvwxyz';
+
+      const decode = (textValue) => {
+        let resultDecode = '';
+        for (let i = 0; i < textValue.length; i++) {
+          if (textValue[i] === ' ') {
+            resultDecode += ' ';
+          } else {
+            const index = alphabet.indexOf(textValue[i]);
+            resultDecode += alphabet[index - 1];
+          }
+        }
+        return resultDecode;
+      }
+      setTekts(decode(textValue));
+      console.log(decode(textValue));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className='App'>
+        <form>
+          <input ref={text} placeholder='Tekst do zaszyfrowania/odszyfrowania'></input>
+          <button onClick={e => Decode(e)} >Odszyfruj</button>
+          <button onClick={e => Code(e)}>Zaszyfruj</button>
+        </form>
+      </div>
+      <div className='Output'>
+        <p>Wynik:</p>
+        <p>{tekst}</p>
+      </div>
+    </>
   );
 }
 
 export default App;
+
+
+// Magda Saturska -  Napisz własną implementację szyfru poligramowego w dowolnej technologii. Użytkownik wprowadza wiadomość i
+// otrzymuje jej szyfrogram lub odszyfrowuje wiadomość. Program może być jako strona internetowa lub okienkowa.
